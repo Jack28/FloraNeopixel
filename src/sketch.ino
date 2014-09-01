231f09c755da814ccffc88b5294a374a076878af
FelixU <f@ai4me.de>
2014-09-01 18:57:04 +0100

    basic serial api

--- src/sketch.ino
+++ src/sketch.ino
@@ -32,26 +32,49 @@ String content = "";
 char character;
 
 void run(String command){
-	// sprint command and arguments
-	String argv[3] = {0};
-	split(command,argv);
+	// 2 3 3 3 --- 11
+	if (command == "show\n" || command == "show\r" || command == "show\0"){
+		strip.show();
+		return;
+	}
 
-	char buf[10]={0};
-	argv[2].toCharArray(buf,argv[2].length());
-	int num=atoi(buf);
-	Serial.println(num);
+	char buf[13]={0};
 
-	if (argv[0] == "on")
-		if (argv[1] == "")
-			colorWipeNum(strip.Color(255,255,255),50,num);
-		if (argv[1] == "red")
-			colorWipeNum(strip.Color(255,0,0),50,num);
-		if (argv[1] == "green")
-			colorWipeNum(strip.Color(0,255,0),50,num);
-	if (argv[0] == "off")
-		colorWipe(strip.Color(0,0,0),50);
-	if (argv[0] == "help")
-		Serial.println("[on] or [off]");
+	command.toCharArray(buf,command.length());
+
+	char ci[3]={0};
+	char cr[4]={0};
+	char cg[4]={0};
+	char cb[4]={0};
+
+	ci[0]=buf[0];
+	ci[1]=buf[1];
+
+	cr[0]=buf[2];
+	cr[1]=buf[3];
+	cr[2]=buf[4];
+
+	cg[0]=buf[5];
+	cg[1]=buf[6];
+	cg[2]=buf[7];
+
+	cb[0]=buf[8];
+	cb[1]=buf[9];
+	cb[2]=buf[10];
+
+	int i,r,g,b;
+
+	i = atoi(ci);
+	r = atoi(cr);
+	g = atoi(cg);
+	b = atoi(cb);
+
+	Serial.println(i);
+	Serial.println(r);
+	Serial.println(g);
+	Serial.println(b);
+
+	strip.setPixelColor(i,strip.Color(r,g,b));
 }
 
 void loop()
@@ -61,6 +84,7 @@ void loop()
 		Serial.print(character);
 		if (character == '\n' || character == '\r' || character == 0){
 			Serial.println(content);
+			content.concat(character);
 			run(content);
 			content="";
 			character=0;
@@ -70,21 +94,6 @@ void loop()
 	}
 }
 
-//void loop() {
-  // Some example procedures showing how to display to the pixels:
-//  colorWipe(strip.Color(255, 0, 0), 50); // Red
-//  colorWipe(strip.Color(0, 255, 0), 50); // Green
-//  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-//  theaterChase(strip.Color(127, 127, 127), 50); // White
-//  theaterChase(strip.Color(127,   0,   0), 50); // Red
-//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
-
-//  rainbow(20);
-//  rainbowCycle(20);
-//  theaterChaseRainbow(50);
-//}
-
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
@@ -92,104 +101,4 @@ void colorWipe(uint32_t c, uint8_t wait) {
       strip.show();
       delay(wait);
   }
-}
-
-// Fill the dots one after the other with a color
-void colorWipeNum(uint32_t c, uint8_t wait, uint8_t num) {
-  uint32_t off = strip.Color(0,0,0);
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, i>=num?off:c);
-      strip.show();
-      delay(wait);
-  }
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-
-void split(String in,String* out){
-	int argc=0;
-	for (int i=0;i<in.length();i++){
-		if (in[i] == ' ' || in[i] == '\n' || in[i] == '\r'){
-			out[argc].concat("\0");
-			argc++;
-		}else{
-			out[argc].concat(in[i]);
-		}
-	}
-	out[argc].concat(" ");
-	for (int i=0;i<3;i++)
-		Serial.println(out[i]);
 }
