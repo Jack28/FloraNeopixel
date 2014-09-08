13f2ba9db54f0b2caf8d2c61543d47165d955e9c
FelixU <f@ai4me.de>
2014-09-08 14:12:14 +0100

    led.py

--- led.py
+++ led.py
@@ -15,37 +15,37 @@ strip.show()
 time = 20
 q=2
 while True:
-#    for k in range(0,3):
-#        strip.dark(1)
-#        for j in range(0,5):
-#            strip.dim(80,0.01,8)
-#            strip.dim(80,0.01,-8)
-#
-#            strip.dim(50,0,50)
-#            sleep(0.05)
-#            strip.dim(50,0,-25)
-#            sleep(0.2)
-#            strip.dim(50,0,50)
-#            sleep(0.05)
-#            strip.dim(50,0,-25)
-#            sleep(0.2)
-#
-#            strip.dark(0.1)
-#            sleep(0.1)
-#        #    strip.randomColor()
-#
-#        for i in range(0,10):
-#            strip.dark((10-i)/10)
-#            sleep(0.05)
-#
-#        strip.dim(50,0,50)
-#        sleep(0.05)
-#        strip.dim(50,0,-25)
-#        sleep(0.2)
-#        strip.dim(50,0,50)
-#        sleep(0.05)
-#        strip.dim(50,0,-25)
-#        sleep(0.2)
-    for k in range(0,10):
-        strip.shift(2,k,(20-k)/10)
+    for k in range(0,3):
+        strip.dark(1)
+        for j in range(0,5):
+            strip.dim(80,0.01,8)
+            strip.dim(80,0.01,-8)
 
+            strip.dim(50,0,50)
+            sleep(0.05)
+            strip.dim(50,0,-25)
+            sleep(0.2)
+            strip.dim(50,0,50)
+            sleep(0.05)
+            strip.dim(50,0,-25)
+            sleep(0.2)
+
+            strip.dark(0.1)
+            sleep(0.1)
+        #    strip.randomColor()
+
+        for i in range(0,10):
+            strip.dark((10-i)/10)
+            sleep(0.05)
+
+        strip.dim(50,0,50)
+        sleep(0.05)
+        strip.dim(50,0,-25)
+        sleep(0.2)
+        strip.dim(50,0,50)
+        sleep(0.05)
+        strip.dim(50,0,-25)
+        sleep(0.2)
+    for k in range(0,200):
+        strip.shift(1)
+        strip.show()
